46c922c909c1f5758e09f55c6e428b07c9d8c682
PedroHenriqueBianchini <ph879102@gmail.com>
2025-09-04 00:29:53 -0300

    arrumando

--- src/main/java/br/com/carstore/servlet/CreateCarServlet.java
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -5,20 +5,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/create-car")
 public class CreateCarServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
 
-        String modelo = request.getParameter("Modelo");
+        String carName = httpServletRequest.getParameter("car-name");
 
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
+        String color = httpServletRequest.getParameter("color");
 
-        String  json = "{\"Modelo\"}" + modelo;
+        httpServletResponse.setContentType("application/json");
 
-        response.getWriter().write(json);
+        httpServletResponse.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = httpServletResponse.getWriter();
+
+        try {
+
+            String json = "{\"name\": \"" + carName + "\",\"color\": \"" + color+ " }";
+
+            httpServletResponse.getWriter().print(json);
+
+        } catch (Exception e) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            httpServletResponse.getWriter().print("{\"error\": \"Failed to read JSON\"}");
+        }
+
+        out.flush();
 
     }
+
 }
